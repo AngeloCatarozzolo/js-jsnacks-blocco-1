f1e8d0252d2b038844b38a2c1b7bbce02f60ad44
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2022-10-26 12:07:42 +0200

    js3

--- jsnack3/script3.js
+++ jsnack3/script3.js
@@ -1,2 +1,15 @@
 //In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
 //chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+const invitati = ['Roberto','Luca','Giovanni','Marco'];
+const checkInvitati = prompt('Inserisci il tuo nome');
+for(let i=0 ; i < invitati.length ; i++){
+    if(checkInvitati.toLowerCase() === invitati[i].toLowerCase()){
+        flag = true;
+    }
+}
+if(flag){
+    console.log('Benvenuto');
+}else{
+    console.log('Attenzione! Non sei stato invitato!')
+}
